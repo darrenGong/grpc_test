bf1f12d0d978ab0cce101d3f9c6e0b367e7335b4
darren.gong <darren_gong@163.com>
2016-12-08 19:23:35 +0800

    add log

--- server/server.go
+++ server/server.go
@@ -18,7 +18,7 @@ const (
 
 
 func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
-	logger.WithField("Server", "HelloWorld").Infof("Start handle function sayhello")
+	logger.WithField("Server", "HelloWorld").Info("Start handle function sayhello")
 	rc := pb.ResponseCode{
 		ErrCode: 0,
 		ErrMessage: "",
@@ -41,6 +41,8 @@ func Init() {
 }
 
 func main() {
+	Init()
+
 	lis, err := net.Listen("tcp", lAddr)
 	if err != nil {
 		logger.WithField("Server", "HelloWorld").Fatalf("Failed to listen [laddr:%s, err:%v]", lAddr, err)
